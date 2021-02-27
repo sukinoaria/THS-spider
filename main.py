d0e7e717d5cfd23a360630fe6d43133c592b0949
yu3yu3 <yu3yu3@outlook.com>
2021-02-27 22:48:21 +0800

    2%

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
+import os
 import time
-
+import random
 import requests
 from bs4 import BeautifulSoup
 
+class Spider:
+    def __init__(self, stock_file, base_url, xls_base_url,xls_download_path="download/", proxy_file=None):
+        self.stocks = [line.strip() for line in open(stock_file,'r').readlines()]
+        self.base_url = base_url
+        self.xls_base_url = xls_base_url
 
-def execute(bizdate, *args, **kwargs):
-    for name, base_url in BASE_URl.items():
-        # 爬取数据的存储路径
-        dt_path = '/data/%s_%s.csv' % (bizdate, name)
+        if not os.path.exists(xls_download_path):
+            os.mkdir(xls_download_path)
+        self.xls_download_path = xls_download_path
 
-        with open(dt_path, "a+") as f:
-            # 记录数据文件的当前位置
-            pos = f.tell()
-            f.seek(0)
-            lines = f.readlines()
-            # 读取文件中的全部数据并将第一列存储下来作为去重依据，防止爬虫意外中断后重启程序时，重复写入相同
-            crawled_list = list(map(lambda line: line.split(",")[0], lines))
-            f.seek(pos)
-            # 循环500次，从第一页开始爬取数据，当页面没有数据时终端退出循环
-            for i in range(1, 500):
-                print("start crawl %s, %s" % (name, base_url % i))
-                web_source = requests.get(base_url % i, headers=REQUEST_HEADER)
-                soup = BeautifulSoup(web_source.content.decode("gbk"), 'lxml')
-                table = soup.select('.J-ajax-table')[0]
-                tbody = table.select('tbody tr')
-                # 当tbody为空时，则说明当前页已经没有数据了，此时终止循环
-                if len(tbody) == 0:
-                    break
-                for tr in tbody:
-                    fields = tr.select('td')
-                    # 将每行记录第一列去掉，第一列为序号，没有存储必要
-                    record = [field.text.strip() for field in fields[1:]]
-                    # 如果记录还没有写入文件中，则执行写入操作，否则跳过这行写入
-                    if record[0] not in crawled_list:
-                        f.writelines([','.join(record) + '\n'])
-                # 同花顺网站有反爬虫的机制，爬取速度过快很可能被封
-                time.sleep(1)
+        self.proxy_pool = None if not proxy_file else \
+            [line.strip() for line in open(proxy_file,'r').readlines()]
 
+        self.REQUEST_HEADER = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
+            'Cookie': 'spversion=20130314; searchGuide=sg; skin_color=black; historystock=300369|*|HK0883|*|HK1810|*|INTC|*|MSFT; usersurvey=1; reviewJump=nojump; Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1614391027,1614413456,1614422866,1614437125; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1614437125; v=A9mbgOPs3XJ9hIHmMChNJeHk6M6wZs0Yt1rxrPuOVYB_Avc4Q7bd6EeqAX6I'
+        }
+
+    def crawl_once(self,stock_code):
+
+        tgt_url = self.base_url.format(stock_code)
+        xls_url = self.xls_base_url.format(stock_code)
+
+        print("start crawl stock:",stock_code)
+        web_source = requests.get(tgt_url, headers=self.REQUEST_HEADER)
+        print(web_source.status_code)
+        if web_source.status_code != "200":
+            pass #todo retry mechanism
+        soup = BeautifulSoup(web_source.content.decode("gbk"), 'lxml')
+        table = soup.select('.J-ajax-table')[0]
+        tbody = table.select('tbody tr')
+        # 同花顺网站有反爬虫的机制，爬取速度过快很可能被封
+        time.sleep(random.randint(4,20))
+
+    def run(self):
+        for code in self.stocks:
+            self.crawl_once(code)
+        print("spider run over ... ")
 
 def t(stock_code):
     base_url = 'http://basic.10jqka.com.cn/'
@@ -61,5 +66,8 @@ def t(stock_code):
 
 
 if __name__ == '__main__':
-    code = "000032"
-    t(code)
+    stock_file = "stocks.txt"
+    base_url = "http://basic.10jqka.com.cn/{}/finance.html#stockpage"
+    xls_base_url = "http://basic.10jqka.com.cn/api/stock/export.php?export=main&type=report&code={}"
+    spider = Spider(stock_file,base_url,xls_base_url)
+    spider.run()
