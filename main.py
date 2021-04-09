c5ebcd5d830f0af9f7a9a29ef866b8692a7e3dad
lunarchx <lunarchx@outlook.com>
2021-04-09 23:05:39 +0800

    finish

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ import os
 import time
 import json
 import random
+import yagmail
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 class Spider:
     def __init__(self, stock_file, base_url,download_path="download/", proxy_file=None):
@@ -18,8 +22,6 @@ class Spider:
         # 数据不完整的代码文件
         self.ignored_file = "ignored_stocks.txt"
 
-
-
         self.base_url = base_url
 
         # 下载路径
@@ -31,11 +33,34 @@ class Spider:
             [line.strip() for line in open(proxy_file,'r').readlines()]
 
         self.REQUEST_HEADER = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36',
-            'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1614327245; spversion=20130314; reviewJump=nojump; searchGuide=sg; historystock=300369%7C*%7C300078%7C*%7C430335; usersurvey=1; Hm_lpvt_78c58f01938e4d85eaf619eae71b4ed1=1614495825; v=A68T7bkZEx3mIBch3iKz2xsWPsi6VAN3nagHasE8S54lEMW-ySSTxq14l77S'
+            'User-Agent': 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50',
+            'Cookie': 'Hm_lvt_78c58f01938e4d85eaf619eae71b4ed1=1614413456,1614422866,1614437125,1614475980; reviewJump=nojump; usersurvey=1; v=A-yuTw5voIQ0YbS4gREgEuQXvcEdpZBOkkmkE0Yt-Bc6UYL3brVg3-JZdKeV'
         }
+
+        # web driver module for getting cookies
+        self.init_webdriver()
         self.init_task()
 
+        #错误提示 发送邮件
+        self.yag = yagmail.SMTP(user='cyyyx233@163.com', host='smtp.163.com')
+
+    def init_webdriver(self):
+        options = Options()
+        options.add_argument(
+            'user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.190 Safari/537.36"')
+
+        options.add_argument("--disable-blink-features")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        # options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # options.add_experimental_option('useAutomationExtension', False)
+        self.driver = webdriver.Chrome(chrome_options=options)
+
+        self.driver.execute_cdp_cmd('Network.setUserAgentOverride', {
+            "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+
+        self.driver.execute_script(
+            "Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
+        print("init web driver ...")
     def init_task(self):
         origin_data = [line.strip() for line in open(self.stock_file,'r').readlines()]
         try:
@@ -48,6 +73,10 @@ class Spider:
             ignored_data = []
         self.stocks = sorted(list(set(origin_data) - set(finished_data) - set(ignored_data)))
 
+    def send_mail(self):
+        contents = ['Spider is stopped by the slipbox, Please do it manually.']
+        self.yag.send('cpxnku@gmail.com', 'ERROR OCCURRED', contents)
+
     def crawl_once(self,stock_code,idx):
         tgt_url = self.base_url.format(stock_code)
         web_source = requests.get(tgt_url, headers=self.REQUEST_HEADER)
@@ -59,9 +88,19 @@ class Spider:
                                                                      stock_code,web_source.status_code))
         if web_source.status_code != 200:
             #todo retry mechanism
-            print("  ....  Status Code ERROR ........")
-            exit(-1)
-        soup = BeautifulSoup(web_source.content.decode("gbk"), 'lxml')
+            print("[ERROR] Status Code ERROR, start chrome for cookies ...")
+            self.driver.get(tgt_url)
+            time.sleep(5)
+            self.REQUEST_HEADER['Cookie'] = self.driver.get_cookies()[0]['value']
+            soup = BeautifulSoup(self.driver.page_source, 'lxml')
+            # 检测 是不是遇到了滑块
+            if not soup.find(id="chartData") and not soup.find(id="finance"):
+                #self.send_mail()
+                print("[WARNING] send mail ...")
+                #time.sleep(120)
+                #soup = BeautifulSoup(self.driver.page_source, 'lxml')
+        else:
+            soup = BeautifulSoup(web_source.content.decode("gbk"), 'lxml')
         # 判断是否爬取到正确的页面： 包含财务诊断的部分，且没有触发同花顺的反爬验证机制
         if soup.find(id="chartData") and soup.find(id="finance"):
             self.get_data(soup,stock_code)
@@ -90,7 +129,10 @@ class Spider:
         assert len(detailed_data) == 5
         for i in range(5):
             cur_type_data = detailed_data[i].find_all(name="li")
-            cur_type = cur_type_data[0].contents[0].attrs['cid']
+            try:
+                cur_type = cur_type_data[0].contents[0].attrs['cid']
+            except:
+                continue
             cur_data = []
             for _li in cur_type_data:
                 cur_data.append(_li.contents[1]) #具体的数值型描述
